5d9fea115b3093779d6a4e4c7654f043e66247e4
grinder <none@none>
2010-05-01 16:24:41 +0300

    Merge (8035)

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1353,6 +1353,8 @@ void GameObject::Use(Unit* user)
                 BattleGround *bg = player->GetBattleGround();
                 if (!bg)
                     return;
+		if (player->GetVehicle())
+                    return;
                 // BG flag click
                 // AB:
                 // 15001
@@ -1377,6 +1379,8 @@ void GameObject::Use(Unit* user)
                 // in battleground check
                 BattleGround *bg = player->GetBattleGround();
                 if (!bg)
+                    return;
+		if (player->GetVehicle())
                     return;
                 // BG flag dropped
                 // WS:
