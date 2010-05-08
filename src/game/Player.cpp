cd08e2abbc6201c669b463cd6c01833a09a51aa6
grinder <none@none>
2010-05-08 12:22:38 +0300

    Merge (8080) Include petsaving under transactioncontrol in Player::SaveToDB()

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -17658,8 +17658,6 @@ void Player::SaveToDB()
     GetSession()->SaveTutorialsData();                      // changed only while character in game
     _SaveGlyphs();
 
-    CharacterDatabase.CommitTransaction();
-
     // check if stats should only be saved on logout
     // save stats can be out of transaction
     if (m_session->isLogingOut() || !sWorld.getConfig(CONFIG_STATS_SAVE_ONLY_ON_LOGOUT))
@@ -17669,6 +17667,8 @@ void Player::SaveToDB()
     // save pet (hunter pet level and experience and all type pets health/mana).
     if (Pet* pet = GetPet())
         pet->SavePetToDB(PET_SAVE_AS_CURRENT);
+
+    CharacterDatabase.CommitTransaction();
 }
 
 // fast save function for item/money cheating preventing - save only inventory and money state
