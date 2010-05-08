93d2c79c863a21ee2d680f8347b16a099ef50bf2
grinder <none@none>
2010-05-08 13:36:11 +0300

    * Fix linux compile * Fix compile error in SpellEffects.cpp

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5890,13 +5890,11 @@ void Spell::EffectScriptEffect(uint32 effIndex)
 						if((*itr).second->GetBase()->GetId() == 54428)
 						(*itr).second->GetBase()->SetDuration(15000);
 					}
-				}
                     return;
+				}
 			}
+			break;
         }
-			
-            break;
-    }
         case SPELLFAMILY_PRIEST:
         {
             switch(m_spellInfo->Id)
