43f9969fc499d9071a916cab9a0b33dc35611d2e
Viste <vistemail@yandex.ru>
2010-05-13 15:23:39 +0300

    remove hack for crashfix and add cool version of crashfix :P

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -2548,7 +2548,7 @@ bool InstanceMap::Add(Player *player)
                                 sLog.outError("GroupBind save players: %d, group count: %d", groupBind->save->GetPlayerCount(), groupBind->save->GetGroupCount());
                             else
                                 sLog.outError("GroupBind save NULL");
-                            return false;
+                            player->RepopAtGraveyard();
                         }
                         // if the group/leader is permanently bound to the instance
                         // players also become permanently bound when they enter
