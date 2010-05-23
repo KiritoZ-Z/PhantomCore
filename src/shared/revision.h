ae644b3ac02ea04b285da87d22df555b5d09ae32
null45 <lexa9691@rambler.ru>
2010-05-23 10:22:53 +0600

    [369] fix cooldown

---
+++ src/shared/revision.h
@@ -0,0 +1,12 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _BUILD_DIRECTIVE "Release"
+ #define _REVISION "*"
+ #define _HASH "*"
+ #define _REVISION_DATE "*"
+ #define _REVISION_TIME "*"
+ #define FILEVER        0,0,0,0
+ #define PRODUCTVER     0,0,0,0
+ #define STRFILEVER     "0, 0, *, *"
+ #define STRPRODUCTVER  "0, 0, *, *"
+#endif // __REVISION_H__
