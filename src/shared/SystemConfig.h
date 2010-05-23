074d82748631a548402ae297d54dc912919ba6ca
Viste <viste02@gmail.com>
2010-05-23 16:46:11 +0300

    packagename fix xD
    Signed-off-by: Viste <viste02@gmail.com>

--- src/shared/SystemConfig.h
+++ src/shared/SystemConfig.h
@@ -27,7 +27,7 @@
 #include "Platform/Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "TrinityCore "
+#define _PACKAGENAME "PhnatomCore "
 #define _CODENAME "LONG_COLD_WINTER"
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
