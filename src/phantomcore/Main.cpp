ed44bfd3a47a67e28cd3830b8c8550c9c7f927aa
Null45 <lexa9691@rambler.ru>
2010-06-05 00:33:04 +0800

    Fix compile again. finishing to fix compile

--- src/phantomcore/Main.cpp
+++ src/phantomcore/Main.cpp
@@ -103,7 +103,6 @@ extern int main(int argc, char **argv)
             {
                 sLog.outError("Runtime-Error: -s option requires an input argument");
                 usage(argv[0]);
-				Log::WaitBeforeContinueIfNeed();
                 return 1;
             }
             if( strcmp(argv[c],"install") == 0)
@@ -122,7 +121,6 @@ extern int main(int argc, char **argv)
             {
                 sLog.outError("Runtime-Error: unsupported option %s",argv[c]);
                 usage(argv[0]);
-				Log::WaitBeforeContinueIfNeed();
                 return 1;
             }
         }
