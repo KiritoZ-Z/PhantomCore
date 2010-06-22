eb9012681fc1b4a7a6023c35e37b63e515100d45
Null45 <lexa9691@rambler.ru>
2010-06-22 19:59:27 +0800

    Add missing scriptnames

---
+++ sql/updates/467_scriptnames.sql
@@ -0,0 +1,12 @@
+UPDATE `instance_template` SET `script`='instance_trial_of_the_crusader' WHERE `map`=649;
+UPDATE `creature_template` SET `ScriptName`='mob_Ice_Puls' WHERE (`entry`='36633');
+UPDATE `creature_template` SET `ScriptName`='mob_Valkyr' WHERE (`entry`='36609');
+UPDATE `creature_template` SET `ScriptName`='mob_ghoul_icc' WHERE (`entry`='37695');
+UPDATE `creature_template` SET `ScriptName`='mob_Defile' WHERE (`entry`='38757');
+UPDATE `creature_template` SET `ScriptName`='mob_RagingSpirit' WHERE (`entry`='36701');
+UPDATE `creature_template` SET `ScriptName`='mob_tirion_icc' WHERE (`entry`='38995');
+UPDATE `creature_template` SET `ScriptName`='mob_tirion_icc' WHERE (`entry`='38995');
+UPDATE `creature_template` SET `ScriptName`='mob_manavoid' WHERE (`entry`='36068');
+UPDATE `creature_template` SET `ScriptName`='mob_colossus' WHERE (`entry`='33237');
+UPDATE `creature_template` SET `ScriptName` = 'npc_manavoid' WHERE (`entry` = '36068');
+UPDATE `creature_template` SET `ScriptName`= 'npc_seaforium_depth_charge' WHERE (`entry`='25401');
