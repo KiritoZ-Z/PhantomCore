51548b6374d1d97971c75d7bc864982b62605e16
Viste <viste02@gmail.com>
2010-07-01 13:09:42 +0300

    clean up part 3

--- sql/updates/8397_world_command.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `command` (name, security, help) VALUES ('instance open', 3, 'Syntax: .instance open mapid [normal|heroic|10normal|10heroic|25normal|25heroic]');
-INSERT INTO `command` (name, security, help) VALUES ('instance close', 3, 'Syntax: .instance close mapid [normal|heroic|10normal|10heroic|25normal|25heroic]');
-
--- src/scripts/northrend/nexus/eye_of_eternity/eye_of_eternity.h
+++
@@ -1,4 +0,0 @@
-#ifndef DEF_EYE_OF_ETERNITY_H
-#define DEF_EYE_OF_ETERNITY_H
-
-#endif
