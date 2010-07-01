4a04db288fd81cb18d8ba2ed7276376a7b5610ce
Viste <viste02@gmail.com>
2010-07-01 13:21:18 +0300

    [2]Glyph of Death Grip fix Signed-off-by: Viste <viste02@gmail.com>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8454,7 +8454,15 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, AuraEffect* trig
                 return false;
             break;
     }
-
+	
+      // Glyph of Death Grip
+      if (auraSpellInfo->Id == 62259)
+      {
+          // remove cooldown of Death Grip
+        if (GetTypeId()==TYPEID_PLAYER)
+            ((Player*)this)->RemoveSpellCooldown(49576, true);
+        return true;
+      } 
     // Blade Barrier
     if (auraSpellInfo->SpellFamilyName == SPELLFAMILY_DEATHKNIGHT && auraSpellInfo->SpellIconID == 85)
     {
