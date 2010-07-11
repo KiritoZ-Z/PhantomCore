9921b548b67de0c37caa6269c1414af20ed8a471
Null45 <lexa9691@rambler.ru>
2010-07-11 17:55:26 +0600

    back old commit: Fix Druid minions damage (Force of Nature)

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -937,8 +937,9 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                 {
                     if (!pInfo)
                         SetCreateHealth(30 + 30*petlevel);
-                    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel * 2.5f - (petlevel / 2)));
-                    SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel * 2.5f + (petlevel / 2)));
+                    int32 bonus_dmg=m_owner->SpellBaseDamageBonus(SPELL_SCHOOL_MASK_NATURE)*0.15f;
+                    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel * 7.5f - (petlevel / 2)+bonus_dmg));
+					SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel * 7.5f + (petlevel / 2)+bonus_dmg));
                     break;
                 }
                 case 15352: //earth elemental 36213
