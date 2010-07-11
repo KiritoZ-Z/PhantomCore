4d329962936a6d908ba621b408aa52049b621315
Kirito <itachi-lee@yandex.ru>
2010-07-11 17:19:48 +0600

    [38] Correction to the accrual of Spell Power to the ground.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10976,7 +10976,7 @@ int32 Unit::SpellBaseHealingBonus(SpellSchoolMask schoolMask)
 
     AuraEffectList const& mHealingDone = GetAuraEffectsByType(SPELL_AURA_MOD_HEALING_DONE);
     for (AuraEffectList::const_iterator i = mHealingDone.begin(); i != mHealingDone.end(); ++i)
-        if (((*i)->GetMiscValue() & schoolMask) != 0)
+        if (((*i)->GetMiscValue() & schoolMask) != 0 || (*i)->GetId() == 55637)
             AdvertisedBenefit += (*i)->GetAmount();
 
     // Healing bonus of spirit, intellect and strength
