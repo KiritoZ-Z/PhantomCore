78fc927a03189e6696558302d45d1ab10577203b
Kirito <itachi-lee@yandex.ru>
2010-07-11 18:07:21 +0600

    typo

--- sql/phantom_upd/39_spell_proc_event.sql
+++
@@ -1,7 +0,0 @@
--- Item - Coliseum 25 Heroic Caster Trinket
-DELETE FROM `spell_proc_event` WHERE `entry` IN (67758);
-INSERT INTO `spell_proc_event` VALUES (67758, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00010000, 0x00000002, 0.000000, 0.000000, 0);
-
--- Item - Coliseum 25 Normal Caster Trinket
-DELETE FROM `spell_proc_event` WHERE `entry` IN (67712);
-INSERT INTO `spell_proc_event` VALUES (67712, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00020000, 0x00000002, 0.000000, 0.000000, 0);
