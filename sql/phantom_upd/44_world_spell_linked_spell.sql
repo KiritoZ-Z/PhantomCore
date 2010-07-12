5d468c80cbf70974715367bb0802d5c69e841971
SignFinder <signfinder@gmail.com>
2010-07-12 12:30:55 +0400

    fix issue 83

---
+++ sql/phantom_upd/44_world_spell_linked_spell.sql
@@ -0,0 +1,7 @@
+INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
+( 54861,-23335, 0, 'Drop Flag on Nitro Boost WSG'),
+( 54861,-23333, 0, 'Drop Flag on Nitro Boost WSG'),
+( 55004,-23335, 0, 'Drop Flag on Nitro Boost WSG'),
+( 55004,-23333, 0, 'Drop Flag on Nitro Boost WSG'),
+( 54861,-34976, 0, 'Drop Flag on Nitro Boost EOS'),
+( 55004,-34976, 0, 'Drop Flag on Nitro Boost EOS');
