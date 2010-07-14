17c714615d674fde110c5a095995cf321369d271
Viste <viste02@gmail.com>
2010-07-14 11:51:30 +0300

    sql for rev[50] Signed-off-by: Viste <viste02@gmail.com>

---
+++ sql/phantom_upd/50_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+-- Item - Paladin T10 Holy 2P Bonus
+DELETE FROM `spell_proc_event` WHERE `entry` IN (70755);
+INSERT INTO `spell_proc_event` VALUES (70755, 0x00, 0x0A, 0x00000000, 0x80000000, 0x00000000, 0x00004000, 0x00010000, 0, 101, 0);
