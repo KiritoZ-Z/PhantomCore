2c5dabcf85c5e16992517c17a9b884261179408f
SignFinder <signfinder@gmail.com>
2010-07-16 10:09:33 +0400

    typo

--- src/server/game/Chat/Commands/Level2.cpp
+++ src/server/game/Chat/Commands/Level2.cpp
@@ -4344,7 +4344,7 @@ bool ChatHandler::HandleWintergraspTimerCommand(const char* args)
     else
         if (1440 < time)
             return false;
-    time *= MINUTE * IN_MILISECONDS;
+    time *= MINUTE * IN_MILLISECONDS;
 
     pvpWG->setTimer((uint32)time);
 
